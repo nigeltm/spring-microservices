7a28d2bcf75933a10527c811963aa5a2cf17b7ed
Nigel Mafukidze <nmafukidze@posb.co.zw>
2022-10-05 14:20:39 +0200

    Implemented the discovery server, registered clients and client side load-balancing

--- order-service/src/main/java/com/ntm/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/ntm/orderservice/OrderServiceApplication.java
@@ -2,11 +2,16 @@ package com.ntm.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
--- order-service/src/main/java/com/ntm/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ntm/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -36,8 +36,8 @@ public class OrderService {
                 .toList();
 
         //Check if item exists in stock before placing the order
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/v1/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/v1/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
